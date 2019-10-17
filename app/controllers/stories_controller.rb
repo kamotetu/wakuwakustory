bd43759abf0a6b113781ffc90cf0d140622be996
kamotetu <lewd_magnifico.m@icloud.com>
2019-10-17 14:48:25 +0900

    #1

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,7 @@ class StoriesController < ApplicationController
 
   def index
     @users = User.all
+    @stories = Story.all
   end
 
   def new
@@ -9,10 +10,18 @@ class StoriesController < ApplicationController
   end
 
   def create
-    @story = Story.new
+    @story = current_user.stories.create(story_params)
+    if @story.save
+      redirect_to root_path
+
+      # flash[:success] = "記事を作成しました"
+      # redirect_to articles_url
+    else
+      render 'stories/new'
+    end
   end
 
-  def task_params
+  def story_params
     params.require(:story).permit(
       :title, 
       :story, 
