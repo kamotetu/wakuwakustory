1587e1a737d413fa4a7c9675f7ccdff0c74aa2c7
kamotetu <lewd_magnifico.m@icloud.com>
2019-10-17 09:50:27 +0900

    #1

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,5 +3,5 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :tags, through: :story_tags
-  has_many :tags, dependent: :destroy
+  has_many :story_tags, dependent: :destroy
 end
