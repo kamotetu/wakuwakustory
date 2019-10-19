f6d1bc144c2dcf8ea4cb813ed18671312a56a814
kamotetu <lewd_magnifico.m@icloud.com>
2019-10-19 10:28:53 +0900

    マイグレーションファイル削除成功

--- db/migrate/20191017005208_tagdestroy.rb
+++
@@ -1,5 +0,0 @@
-class Tagdestroy < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :tags
-  end
-end
--- db/migrate/20191017005326_atory_tagsdestroy.rb
+++
@@ -1,5 +0,0 @@
-class AtoryTagsdestroy < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :story_tags
-  end
-end
