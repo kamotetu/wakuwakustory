60b150c26fbb08558e79f12cd1bdb5cb207b38d8
kamotetu <lewd_magnifico.m@icloud.com>
2019-10-26 08:31:34 +0900

    ロゴテスト

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,6 @@
 class StoriesController < ApplicationController
 
   def index
-    # @users = User.order("created_at DESC").page(params[:page]).per(5)
     @stories = Story.order("created_at DESC").page(params[:page]).per(5)
   end
 
