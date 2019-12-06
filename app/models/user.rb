1e5dc6b58aec4f771c980e188c2cbe9b189cc395
kamotetu <lewd_magnifico.m@icloud.com>
2019-12-06 15:19:49 +0900

    メール機能一旦置いとく

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :stories, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :maintitles, dependent: :destroy
