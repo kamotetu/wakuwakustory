d7cdfe72ac7483ea532e74a65b3c2a85c468dbdf
kamotetu <lewd_magnifico.m@icloud.com>
2019-12-06 11:51:47 +0900

    テスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,22 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+  
+  mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "kamo7290@gmain.com",
+    :password => "08051082214",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                       sessions: 'users/sessions' }
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   root "stories#index"
-
   get 'userindex' => 'searches#userindex'
   get 'maintitleindex' => 'searches#maintitleindex'
   get 'genreindex' => 'searches#genreindex'
