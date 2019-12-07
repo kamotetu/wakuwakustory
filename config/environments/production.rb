c3de73e2e1c69710da6fa4cda893b4178244316d
kamotetu <lewd_magnifico.m@icloud.com>
2019-12-07 13:05:06 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,17 @@ Rails.application.configure do
   # mail setting
   config.action_mailer.default_url_options = { host: 'http://54.65.254.36/' }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "kamo7290@gmail.com",
+    :password => "08051082214",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
