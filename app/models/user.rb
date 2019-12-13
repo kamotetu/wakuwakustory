1af8a40730113965cb0602939215d71bb6afb3e1
kamotetu <lewd_magnifico.m@icloud.com>
2019-12-13 13:33:22 +0900

    ローカルでは完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :omniauthable,omniauth_providers: %i[facebook google_oauth2]
   has_many :stories, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :maintitles, dependent: :destroy
@@ -14,5 +15,49 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # binding.pry
+
+    if snscredential.present? #sns登録のみ完了してるユーザー
+      user = User.where(id: snscredential.user_id).first
+      unless user.present? #ユーザーが存在しないなら
+        user = User.new(
+          # snsの情報
+          # binding.pry # => auth.infoとかで確認 
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+      #binding.pry
+
+    else #sns登録 未
+      user = User.where(email: auth.info.email).first
+      if user.present? #会員登録 済
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else #会員登録 未
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+        # binding.pry
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+        # binding.pry 
+      end
+    end
+    # binding.pry
+    # hashでsnsのidを返り値として保持しておく
+    return { user: user , sns_id: sns.id }
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
-                                      sessions: 'users/sessions' }
+                                      sessions: 'users/sessions',
+                                      omniauth_callbacks: 'users/omniauth_callbacks' }
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   root "stories#index"
